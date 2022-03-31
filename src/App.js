69d66787523b8946e5219bbc89c9e24e8a8e8da7
Ben Ong <ben_ong@yahoo.com>
2022-03-31 00:32:34 -0700

    Removed unnecessary google api code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
 import logo from './logo.svg';
 import './App.css';
 import LoginBtn from "./components/login";
@@ -7,19 +5,7 @@ import LoginBtn from "./components/login";
 const clientId = "909203397537-6li43mot3oecheeh4asel1gsh60lgmjk.apps.googleusercontent.com"
 
 function App() {
-  //-- Initialize client
-  useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
-
-    //-- Load client
-    gapi.load('client:auth2', start); 
-  });
-
+  
   return (
       <div className="App">
         <LoginBtn />
