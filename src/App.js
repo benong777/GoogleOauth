7c6c07409e7d919b407228d3b58bf723c81ac3ab
Ben Ong <ben_ong@yahoo.com>
2022-03-31 01:31:20 -0700

    Added Melinda's login html and css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,32 @@ function App() {
   
   return (
       <div className="App">
-        <LoginBtn />
+          <div className="container">
+              <div className="card">
+                  <h1>Log in</h1>
+                  <div className="log-in">
+                  <input type="text" placeholder="County" list="counties"></input>
+                  <datalist id="counties">
+                      <option value="Stanislaus County"></option>
+                      <option value="San Joaquin County"></option>
+                      <option value="Merced County"></option>
+                      <option value="Tuolumne County"></option>
+                      <option value="Madera County"></option>
+                  </datalist>
+                  <input type="text" placeholder="Unit Number" list="units"></input>
+                  <datalist id="units">
+                      <option value="Unit 101"></option>
+                      <option value="Unit 102"></option>
+                      <option value="Unit 103"></option>
+                      <option value="Unit 104"></option>
+                  </datalist>
+
+
+                      {/* <button>Sign In with Google</button> */}
+                      <LoginBtn className="login-button"/>
+                  </div>
+              </div>
+          </div>
       </div>
   );
 }
